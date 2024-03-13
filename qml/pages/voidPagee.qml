396cf66f630f074f8a5a5632bf064e10846d03bc
Akapulka00 <svavolhov@gmail.com>
2024-03-13 18:09:01 +0300

    first commit

---
+++ qml/pages/voidPagee.qml
@@ -0,0 +1,14 @@
+import QtQuick 2.0
+import Sailfish.Silica 1.0
+
+
+
+Page {
+    allowedOrientations: Orientation.All
+
+    PageHeader {
+        objectName: "pageHeader"
+        title: qsTr("Вы успешно прошли регистрацию!!!")
+    }
+}
+
---
+++ translations/ru.auroraos.Teast.ts
@@ -0,0 +1,86 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="45"/>
+        <source>Регистрация</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="53"/>
+        <source>Фамилия</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="60"/>
+        <source>Имя</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="67"/>
+        <source>Отчество</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="75"/>
+        <source>Электронная почта</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="96"/>
+        <source>Зарегистрация</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DefaultCoverPage</name>
+    <message>
+        <location filename="../qml/cover/DefaultCoverPage.qml" line="46"/>
+        <source>Template</source>
+        <translation>Template</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="64"/>
+        <source></source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="48"/>
+        <source>Регистрация</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="58"/>
+        <source>Введите номер телефона </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="69"/>
+        <source>Введите Логин</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="74"/>
+        <source>Зарегистрироваться</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="83"/>
+        <source>Вы ввели не валидный номер!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>voidPagee</name>
+    <message>
+        <location filename="../qml/pages/voidPagee.qml" line="11"/>
+        <source>Вы успешно прошли регистрацию!!!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
